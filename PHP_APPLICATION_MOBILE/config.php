bd5b6a6cc7b7b6b0939a0ab1fc8ffa5d0b7a864f
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-01-03 16:57:25 +0100

    comit

---
+++ PHP_APPLICATION_MOBILE/config.php
@@ -0,0 +1,7 @@
+<?php
+// Configuration de la base de données
+$servername = "localhost";
+$dbname = "APPLICATION_CAMPING";
+$dbusername = "adrien_camping";
+$dbpassword = "nu9J556~z";
+?>
--- PHP_APPLICATION_MOBILE/inscription.php
+++ PHP_APPLICATION_MOBILE/inscription.php
@@ -3,15 +3,18 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Configuration de la base de données
-$servername = "localhost";
-$dbname = "APPLICATION_CAMPING";
-$dbusername = "adrien_camping";
-$dbpassword = "nu9J556~z";
+// Inclure PHPMailer dans votre script
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Connexion à la base de données
+// Importer PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Se connecter à la base de données (Inclure les informations de connexion depuis le fichier de configuration)
+require_once 'config.php';
+
+// Se connecter à la base de données
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-
 if ($conn->connect_error) {
     die("La connexion à la base de données a échoué : " . $conn->connect_error);
 }
@@ -43,6 +46,43 @@ if ($result->num_rows > 0) {
     echo json_encode(['status' => 'error', 'message' => 'Email déjà utilisé']);
     $stmt->close();
     $conn->close();
+    exit();
+}
+
+//Lien de confirmation
+$base_url = "http://vaca-meet.fr"; // votre domaine actuel.
+$confirmation_link = $base_url . '/PHP/APPLICATION_MOBILE/confirm_Token.php?token=' . $confirm_token;
+
+
+// Envoyer un e-mail de confirmation
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = 0; // Désactiver le débogage SMTP
+try {
+    // Configurer les paramètres du serveur SMTP
+    $mail->isSMTP();                                     
+    $mail->Host = 'smtp.ionos.fr';                      
+    $mail->SMTPAuth = true;                              
+    $mail->Username = 'adrien-pago@vaca-meet.fr'; 
+    $mail->Password = 'RG3SrzY7PhvnWQh';              
+    $mail->SMTPSecure = 'tls';                           
+    $mail->Port = 587;
+
+  
+    // Configurer les paramètres de l'e-mail
+    $mail->setFrom('adrien-pago@vaca-meet.fr', 'Support Technique');
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+
+    $mail->Subject = 'Confirmez votre compte';
+    $mail->Body    = 'Bienvenue sur notre application Camping! Veuillez cliquer sur le lien suivant pour confirmer votre compte : <a href="' . $confirmation_link . '">Confirmer mon compte</a>';
+
+     // Envoyer l'e-mail
+     $mail->send();
+ }  catch (Exception $e) {
+    $response_array['status'] = 'error';
+    $response_array['message'] = "Le message n'a pas pu être envoyé. Erreur : {$mail->ErrorInfo}";
+    echo json_encode($response_array);
+    die();
     exit();
 }
 
