28877bdd4191ff7d66293e29fdf2eab8226b73fd
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-03-22 11:23:17 +0100

    Delete AppStyles.d.ts

--- Styles/AppStyles.d.ts
+++
@@ -1,8 +0,0 @@
-// AppStyles.d.ts
-
-// Déclarer les types pour les styles utilisés dans AppStyles.js
-declare module '*.scss' {
-    const styles: { [key: string]: string };
-    export default styles;
-  }
-  
